9f28dbeae0ed16949ea91593589d45ea9c339fa1
Akanksha <228480495+arpitgit2006@users.noreply.github.com>
2026-08-27 13:57:08 +0530

    .trim method

--- js/app.js
+++ js/app.js
@@ -1,16 +1,4 @@
-console.log("let's go");
-let a=10;
-let b=20;
-console.log(a+b,a-b,a*b);
-console.log(`sum :${a+b}, diff. :${a-b}, product :${a*b}`);
-console.log(`a>b ${a>b}`);
-console.log(`a<b ${a<b}`);
-console.log(a===b);
-console.log(a==="10");
-if(a<b){
-    console.log("a is smaller than b");   
-}
-console.error("this is an error");
-console.warn("this is an error");
-let name= prompt("enter name");
-console.log(name);
+let a="   Arpit  ";
+console.log(a);
+console.log(a.trim());
+ 

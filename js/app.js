338c5865e643a054a3b2e0a55df03025dc8f5b8a
Akanksha <228480495+arpitgit2006@users.noreply.github.com>
2026-08-31 19:51:46 +0530

    filter in map

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-let arr=[1,2,3,4,5];
+let arr=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
 console.log(arr);
-function multiplyby2(el){
-    return el*2;
+function even(el){
+    return (el%2==0);
 }
-let double=arr.map(multiplyby2);
+let double=arr.filter(even);
 console.log(double);

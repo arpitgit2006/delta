2c26d8a8b4179a2e854fe4adc4f6eb17d346fa0a
Akanksha <228480495+arpitgit2006@users.noreply.github.com>
2026-08-31 19:53:40 +0530

    even no. by filter

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 let arr=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
 console.log(arr);
-function even(el){
+let double=arr.filter((el) => {
     return (el%2==0);
-}
-let double=arr.filter(even);
+});
 console.log(double);

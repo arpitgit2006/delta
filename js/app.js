531c3a9ae35ea1a540e5fc4227dbcc09ea5b64b7
Akanksha <228480495+arpitgit2006@users.noreply.github.com>
2026-09-01 20:17:30 +0530

    innertext and innerhtml

--- js/app.js
+++ js/app.js
@@ -1,2 +1,5 @@
 let obj = document.getElementById("main_img");
 obj.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRl3LwzNum_TP46r3exVSxLhhikSc8FEEqTQdFZ81n4KNp2yekEiPy90ghN&s=10";
+let pa=document.getElementById("para");
+console.dir(pa.innerText);
+console.dir(pa.innerHTML);

dbd11e413a4be199905ff49fef76224b627df45c
Akanksha <228480495+arpitgit2006@users.noreply.github.com>
2026-09-03 23:07:25 +0530

    getter and setter

--- js/app.js
+++ js/app.js
@@ -2,4 +2,11 @@ let obj = document.getElementById("main_img");
 obj.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRl3LwzNum_TP46r3exVSxLhhikSc8FEEqTQdFZ81n4KNp2yekEiPy90ghN&s=10";
 let pa=document.getElementById("para");
 console.dir(pa.innerText);
-console.dir(pa.innerHTML);
+console.dir(pa.innerHTML); 
+let box=document.querySelector('#box')
+let input=document.querySelector('#input');
+let button=document.querySelector('#button');
+button.addEventListener('click',() => { console.log(input.value); })
+let si=document.querySelector("#main_img");
+si.getAttribute("id");
+si.setAttribute("id","spidermanimg");

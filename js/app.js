bdad9de766f22dc0ee60a7f3304b6282c2d5b767
Akanksha <228480495+arpitgit2006@users.noreply.github.com>
2026-09-21 22:32:19 +0530

    query selector

--- js/app.js
+++ js/app.js
@@ -2,3 +2,5 @@ let imgobj=document.getElementById("main_img");
 console.log(imgobj);
 console.dir(imgobj);
 imgobj.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIaIb5aHdB6B2oMVoz2mBPbhwOmSfKL4jCVGGttT47tA&s=10";
+let pobj=document.querySelector("#para");
+pobj.innerHTML="testing";

20b894601e44782589ba01999e3c7b0c91a1acfb
Akanksha <228480495+arpitgit2006@users.noreply.github.com>
2026-09-21 22:09:54 +0530

    try catch

--- js/app.js
+++ js/app.js
@@ -1,17 +1,9 @@
-let obj = document.getElementById("main_img");
-obj.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRl3LwzNum_TP46r3exVSxLhhikSc8FEEqTQdFZ81n4KNp2yekEiPy90ghN&s=10";
-let pa=document.getElementById("para");
-console.dir(pa.innerText);
-console.dir(pa.innerHTML); 
-let box=document.querySelector('#box')
-let input=document.querySelector('#input');
-let button=document.querySelector('#button');
-button.addEventListener('click',() => { console.log(input.value); })
-let si=document.querySelector("#main_img");
-si.getAttribute("id");
-si.setAttribute("id","spidermanimg");
-pa.style.color="pink";
-let heading=document.querySelector("h1");
-heading.classList.add("underline");
-let h2=document.querySelector("h2");
-h2.remove();
+console.log("hello");
+console.log('hello');
+try{
+console.log(a);
+} catch{
+    console.log('error occured, a is not initialized')
+}
+console.log("hello");
+console.log("hello");

ee7a3087a284df5c308eec89aa6fbf55c00a99c6
Akanksha <228480495+arpitgit2006@users.noreply.github.com>
2026-09-22 23:24:38 +0530

    addEvent Listener

--- js/app.js
+++ js/app.js
@@ -1,9 +1,6 @@
 let btn=document.querySelectorAll("button");
 for(b of btn){
-    b.onclick=btnclick;
-    b.onmouseenter=function () {
-        console.log('mouse has entered!');
-    }
+    b.addEventListener("click",btnclick);
 }
 function btnclick(){
     alert("button is clicked!");

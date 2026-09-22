46864775da7316c2813ab312942e6dcc6b477bdf
Akanksha <228480495+arpitgit2006@users.noreply.github.com>
2026-09-22 23:13:21 +0530

    on mouse enter

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
-let btn=document.querySelector("button");
-btn.onclick= function (){
-    console.log("button is clicked!");
+let btn=document.querySelectorAll("button");
+for(b of btn){
+    b.onclick=btnclick;
+    b.onmouseenter=function () {
+        console.log('mouse has entered!');
+    }
+}
+function btnclick(){
+    alert("button is clicked!");
 }

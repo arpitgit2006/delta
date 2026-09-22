11ee05415e18c5866fc5c266fa284080f8bc36a4
Akanksha <228480495+arpitgit2006@users.noreply.github.com>
2026-09-22 23:42:47 +0530

    generate random color

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
-let btn=document.querySelectorAll("button");
-for(b of btn){
-    b.addEventListener("click",btnclick);
+function genran(){
+    let r=Math.floor(Math.random()*255);
+    let g=Math.floor(Math.random()*255);
+    let b=Math.floor(Math.random()*255);
+    let color = `rgb(${r},${g},${b})`;
+    return color;
 }
-function btnclick(){
-    alert("button is clicked!");
-}
+let btn = document.querySelector("button");
+btn.addEventListener("click",function(){
+    let ran=genran();
+    let heading=document.querySelector("h3");
+    heading.innerText=ran;
+    let clr=document.querySelector("div");
+    clr.style.backgroundColor=ran;
+});
